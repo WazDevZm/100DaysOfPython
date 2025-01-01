3610b9c5e5dbdb8f52e2aa963258490f239e36d1
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-01-01 10:25:59 +0200

    Day1

--- SpellingChecker App.py
+++ SpellingChecker App.py
@@ -8,8 +8,8 @@ root.title("Wazingwa's Spelling Checker")
 root.geometry("700x400")
 root.config(background="grey")
 
-
-
+heading = Label(root, text="Wazingwa's Spelling Checker", font=("Trebuchet MS", 20, "bold"), bg="#dae6f6", fg="#364971")
+heading.pack(pady=(50))
 
 
 root.mainloop()
