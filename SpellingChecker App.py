5ce2ec1c460eb33a2a8b31fb08d34762b138cce9
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-01-01 10:22:35 +0200

    Day1

---
+++ SpellingChecker App.py
@@ -0,0 +1,15 @@
+import tkinter
+from tkinter import *
+from textblob import TextBlob
+
+
+root = Tk()
+root.title("Wazingwa's Spelling Checker")
+root.geometry("700x400")
+root.config(background="grey")
+
+
+
+
+
+root.mainloop()
