e13f63a1e6109a75c881cbe78a10d9bd3fe22a32
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-01-03 08:55:05 +0200

    Day3

--- To-doList App.py
+++ To-doList App.py
@@ -19,14 +19,34 @@ heading.place(x=100, y= 20)
 
 
 #main
-frame =  Frame(box, width=400, height=50, bg="grey")
+frame =  Frame(box, width=400, height=50, bg="white")
 frame.place(x=0, y=180)
 
 #taskStringVar()
 task_entry = Entry(frame, width = 18, font="arial 20", bd=0)
 task_entry.place(x=10, y=7)
-box.mainloop()
+task_entry.focus()
 
 
-button = Button(frame, text = "ADD", font = "arial 20 bold", width=6, bg="blue",fg="white", bd=0)
+
+button = Button(frame, text = "ADD", font = "arial 20 bold", width=6, bg="#5a95ff",fg="#fff", bd=0)
 button.place(x=300, y=0)
+
+#listbox
+frame1= Frame(box, bd=3, width=700, height=280, bg="#32405b")
+frame1.pack(pady=(160,0))
+
+
+listbox =Listbox(frame1, font=('arial', 12), width=40, height=16, bg="#32405b")
+listbox.pack(side=LEFT, fill=BOTH)
+
+
+
+
+
+
+
+
+
+
+box.mainloop()
