5e7e8953a007dc8cb458991746db3b1934fe7811
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-01-05 15:10:15 +0200

    Addes the listbox, not overlapping

--- To-doList App.py
+++ To-doList App.py
@@ -1,7 +1,7 @@
 import tkinter
 from tkinter import *
 
-#This is Wazingwa's To -Do List app being made using Python from scratch
+# This is Wazingwa's To-Do List app being made using Python from scratch
 box = Tk()
 box.title("My To-do App")
 box.geometry("400x650+400+100")
@@ -9,44 +9,29 @@ box.resizable(False, False)
 
 task_list = []
 
-#Image_icon = PhotoImage(file="Images/waz.png")
-#box.iconphoto(False, Image_icon)
+# Top bar
+heading = Label(box, text="All my tasks!", font="arial 20 bold", fg="black")
+heading.place(x=100, y=20)
 
-#top bar
-heading = Label(box, text = "All my tasks!", font="arial 20 bold", fg="black")
-heading.place(x=100, y= 20)
+# Entry frame
+frame = Frame(box, width=400, height=50, bg="white")
+frame.place(x=0, y=80)
 
-
-
-#main
-frame =  Frame(box, width=400, height=50, bg="white")
-frame.place(x=0, y=180)
-
-#taskStringVar()
-task_entry = Entry(frame, width = 18, font="arial 20", bd=0)
+# Task entry field
+task_entry = Entry(frame, width=18, font="arial 20", bd=0)
 task_entry.place(x=10, y=7)
 task_entry.focus()
 
-
-
-button = Button(frame, text = "ADD", font = "arial 20 bold", width=6, bg="#5a95ff",fg="#fff", bd=0)
+# Add button
+button = Button(frame, text="ADD", font="arial 20 bold", width=6, bg="#5a95ff", fg="#fff", bd=0)
 button.place(x=300, y=0)
 
-#listbox
-frame1= Frame(box, bd=3, width=700, height=280, bg="#32405b")
-frame1.pack(pady=(160,0))
+# Listbox frame
+frame1 = Frame(box, bd=3, width=400, height=400, bg="#32405b")
+frame1.place(x=0, y=150)
 
-
-listbox =Listbox(frame1, font=('arial', 12), width=40, height=16, bg="#32405b")
+# Listbox
+listbox = Listbox(frame1, font=('arial', 12), width=40, height=20, bg="#32405b", fg="#fff")
 listbox.pack(side=LEFT, fill=BOTH)
-
-
-
-
-
-
-
-
-
 
 box.mainloop()
